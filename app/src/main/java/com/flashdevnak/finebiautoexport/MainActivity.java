8f2b5ed1d1cee1ce486e3c2aa9ca96505422ecc0
Flashdevnak <26nak.testdev@gmail.com>
2026-09-13 05:32:25 +0700

    fix: track FineBI workspace visibility during recovery

--- app/src/main/java/com/flashdevnak/finebiautoexport/MainActivity.java
+++ app/src/main/java/com/flashdevnak/finebiautoexport/MainActivity.java
@@ -32,7 +32,14 @@ public final class MainActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+        AppVisibility.setDailyVisible(true);
         refreshSessionStatus();
+    }
+
+    @Override
+    protected void onPause() {
+        AppVisibility.setDailyVisible(false);
+        super.onPause();
     }
 
     @Override
